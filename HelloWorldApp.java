a14fc531cea9ad7a40ed9eb498fb91793692bd56
unknown <mursyidnaufal@gmail.com>
2015-09-12 21:08:13 +0700

    INI HELLO

---
+++ HelloWorldApp.java
@@ -0,0 +1,7 @@
+public class HelloWorldApp{
+public static void main(String[]args){
+System.out.println("Selamat belajar java :) ");
+System.out.println("Muhammad Murysid Naufal");
+System.out.print("Selamat belajar java :)\n");
+}
+}
